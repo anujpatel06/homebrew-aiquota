97bc37d933d81087e00f59b0f264b1dc700c0fdd
Anuj Patel <73183802+anujpatel06@users.noreply.github.com>
2026-09-09 22:33:40 +0530

    aiquota 0.4.1

--- Formula/aiquota.rb
+++ Formula/aiquota.rb
@@ -3,8 +3,8 @@ class Aiquota < Formula
 
   desc "See how much of every AI subscription you've used, in one place"
   homepage "https://github.com/anujpatel06/aiquota"
-  url "https://files.pythonhosted.org/packages/fe/d6/0facd67113a3c4216bdd83b869f7932192c91177856ce557877fd83807b5/aiquota-0.4.0.tar.gz"
-  sha256 "92124848ed198e78d71f13187bf76a50b897e64782647bcbd952471347a85765"
+  url "https://files.pythonhosted.org/packages/ae/f4/0df5e199fd514a21cad3f5f3034d49c27955cd974c966b779887e2616310/aiquota-0.4.1.tar.gz"
+  sha256 "99edb0234accd89042645324c10f6f2251feacd294873ad5af6badd53b5585d5"
   license "MIT"
 
   depends_on "python@3.12"
